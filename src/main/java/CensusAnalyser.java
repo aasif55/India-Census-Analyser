ad31b960c8d555fcb71c44efde7131a3dcab93c6
your name <exam@mail.com>
2021-04-08 02:01:43 +0530

    Added: Census Analyser

---
+++ src/main/java/CensusAnalyser.java
@@ -0,0 +1,7 @@
+package IndianStatesCensusAnalyser;
+
+public class CensusAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Indian States Census Analyser");
+    }
+}
